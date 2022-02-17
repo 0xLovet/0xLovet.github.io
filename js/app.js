a1314cab56a715360aad593f92e1e87902f16ce1
0xLovet <98976541+0xLovet@users.noreply.github.com>
2022-02-17 12:11:35 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,8 +25,7 @@ var maxTokenWallet;
 var tokenBalance;
 var maxMintable;
 var paused;
-var web3;
-var nft_contract;
+
 
 const sleep = (milliseconds) => {
 	return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -36,12 +35,6 @@ const isMetaMaskInstalled = () => {
   const { ethereum } = window;
   return Boolean(ethereum && ethereum.isMetaMask);
 };
-
-const MetaMaskClientCheck = () => {
-	if (!isMetaMaskInstalled()) {
-		connectButton.innerHTML = "Please install MetaMask!";
-	} else {connectButton.innerHTML = "CONNECT";}
-	};
 
 const smallAddressFormat = () => {
 	var addr="";	
@@ -87,67 +80,161 @@ const initialize = () => {
 
 	getBG();
 	countdown();
-
-	MetaMaskClientCheck();
-}
 	
+	const web3 = new Web3(window.ethereum);
+	const nft_contract = new web3.eth.Contract(nft_abi, NFT_ADDRESS);
+	
+	const MetaMaskClientCheck = () => {
+	if (!isMetaMaskInstalled()) {
+		connectButton.innerHTML = "Please install MetaMask!";
+	} else {connectButton.innerHTML = "CONNECT";}
+	};
 
-/* CONNECT button */
-connectButton.onclick = async () => {
-	connectButton.disabled=true;
-	web3 = new Web3(window.ethereum);
-	nft_contract = new web3.eth.Contract(nft_abi, NFT_ADDRESS);
-	try {
-		//Login
-		await ethereum.request({ method: 'eth_requestAccounts' });
-		const chainId = await ethereum.request({ method: 'eth_chainId' });
+	/* CONNECT button */
+	connectButton.onclick = async () => {
+		connectButton.disabled=true;
+		try {
+			//Login
+			await ethereum.request({ method: 'eth_requestAccounts' });
+			const chainId = await ethereum.request({ method: 'eth_chainId' });
 
-		//Get chain id 
-		if(chainId == CHAIN_ID ){ 
-			connectButton.innerHTML = "Polygon";
-			//Display address
-			connectButton.innerHTML =  smallAddressFormat();
+			//Get chain id 
+			if(chainId == CHAIN_ID ){ 
+				connectButton.innerHTML = "Polygon";
+				//Display address
+				connectButton.innerHTML =  smallAddressFormat();
 
-			//Check cost and maxMintable then enable the right button
-			await refresh();		
+				//Check cost and maxMintable then enable the right button
+				await refresh();		
+			}
+			else{ 
+				connectButton.innerHTML = "use Polygon";
+			}
+			
+		} catch (error) {
+			console.error(error);
+			connectButton.innerHTML = "Error...";
 		}
-		else{ 
-			connectButton.innerHTML = "use Polygon";
-		}
+		
+	};
 
-	} catch (error) {
-		console.error(error);
-		connectButton.innerHTML = "Error...";
+	/* APPROVE WETH */
+	approveButton.onclick = async () => {
+		approveButton.disabled=true;
+
+		const response = await refresh();
+		if( ethereum.selectedAddress.length > 0 && response){
+			
+			//Get cost
+			nft_contract.methods.cost().call().then(async function (result) {
+				cost = BigInt(result) *  BigInt(mintAmount);
+				
+				//Approve
+				const encodedFunction = web3.eth.abi.encodeFunctionCall(token_approve_abi, [NFT_ADDRESS, cost]);
+				const transactionParameters = {
+					to: TOKEN_ADDRESS,
+					from: ethereum.selectedAddress,
+					data: encodedFunction
+				};          
+
+				// As with any RPC call, it may throw an error
+				try {
+					const txHash = await ethereum.request({
+					method: 'eth_sendTransaction',
+					params: [transactionParameters],
+					});
+					img.data = "images/logo_animated.svg";
+					textInfo.innerHTML = "Waiting tx...";
+					
+					//WAITING FOR SUCCESS TX
+					let txReceipt = null;
+					while (txReceipt == null) { // Waiting expectedBlockTime until the transaction is mined
+						txReceipt = await web3.eth.getTransactionReceipt(txHash);
+						await sleep(2000)
+					}
+					if (txReceipt.status){		
+						mintButton.disabled = false;
+						approved = true;
+						img.data = "images/lovet.svg";
+						textInfo.innerHTML = "Tx confirmed!<br>Click MINT to get your " + mintAmount + " $LOVET."
+					}
+					else{
+						textInfo.innerHTML = "Something went wrong, check the transaction or retry."
+						approveButton.disabled = false;
+					}
+				} catch (error){
+					console.log(`Error: ${error.message}`);
+					textInfo.innerHTML = "Something went wrong, check the transaction or retry."
+					approveButton.disabled = false;
+				}
+			});	
+		} else{
+			console.log("Error no address no mintAmount");
+			approveButton.disabled = false;
+		}    		
+	};
+
+	/* Load metadata & image from IPFS */
+	const get_img = async (tokenId) => {
+		//Get metadata from contract
+		nft_contract.methods.tokenURI(tokenId).call().then( async function (result) {
+			const json_URL = IPFS_GATEWAY + result.substring(7);
+
+			//Get image from IPFS
+			try{
+				$.getJSON(json_URL, function(data) {
+					var img_URL = data.image;
+					const image_URI = IPFS_GATEWAY + img_URL.substring(7);		
+					textInfo.innerHTML = "Here is what you get: " + data.name + "<br>" +
+											"Type: " + data.attributes[0].value + ", Area: " + data.attributes[1].value  + "<br>" +
+											"I'm loading the image...";
+					//Load image
+					downloadingImage.onload = function() {
+						imgLink.href = image_URI;
+						img.data = this.src;
+						textInfo.innerHTML = "Here is what you get: " + data.name + "<br>" +
+												"Type: " + data.attributes[0].value + ", Area: " + data.attributes[1].value  + "<br>" +
+												"Done! View on OpenSea";
+						//Enable another mint
+						if (cost > 0){
+							approveButton.disabled = false;
+						}
+						else{
+							mintButton.disabled = false;
+						}
+					};			
+					downloadingImage.src = image_URI;
+				});
+			}catch(error){
+				console.log(`Error: ${error.message}`);
+				textInfo.innerHTML = "Sorry, something went wrong, check on OpenSea.";
+				img.data = "images/lovet.svg";
+			}	
+		});		
 	}
 
-};
-
-/* APPROVE WETH */
-approveButton.onclick = async () => {
-	approveButton.disabled=true;
-
-	const response = await refresh();
-	if( ethereum.selectedAddress.length > 0 && response){
-
-		//Get cost
-		nft_contract.methods.cost().call().then(async function (result) {
-			cost = BigInt(result) *  BigInt(mintAmount);
-
-			//Approve
-			const encodedFunction = web3.eth.abi.encodeFunctionCall(token_approve_abi, [NFT_ADDRESS, cost]);
+	/* MINT */
+	mintButton.onclick = async () => {
+		mintButton.disabled=true;
+		var tokenId;
+		
+		const response = await refresh();
+		if( ethereum.selectedAddress.length > 0 && response){
+			//mint
+			const encodedFunction = web3.eth.abi.encodeFunctionCall(nft_mint_abi, [mintAmount]);
 			const transactionParameters = {
-				to: TOKEN_ADDRESS,
+				to: NFT_ADDRESS,
 				from: ethereum.selectedAddress,
 				data: encodedFunction
-			};          
-
+			};              
+			
 			// As with any RPC call, it may throw an error
 			try {
 				const txHash = await ethereum.request({
 				method: 'eth_sendTransaction',
 				params: [transactionParameters],
 				});
-				img.data = "images/logo_animated.svg";
+				img.data = "images/logo_animated.svg";	
 				textInfo.innerHTML = "Waiting tx...";
 
 				//WAITING FOR SUCCESS TX
@@ -156,224 +243,135 @@ approveButton.onclick = async () => {
 					txReceipt = await web3.eth.getTransactionReceipt(txHash);
 					await sleep(2000)
 				}
-				if (txReceipt.status){		
-					mintButton.disabled = false;
-					approved = true;
-					img.data = "images/lovet.svg";
-					textInfo.innerHTML = "Tx confirmed!<br>Click MINT to get your " + mintAmount + " $LOVET."
+
+				//GET TOKEN ID FROM TX
+				if (txReceipt.status){
+					textInfo.innerHTML="Your tx has been confirmed! I'm loading your new NFT...";				
+					
+					//Get tokenId
+					if ( cost > 0 ) {
+						tokenId = parseInt(txReceipt.logs[2].topics[3],16);
+					}
+					else{
+						tokenId = parseInt(txReceipt.logs[0].topics[3],16);
+					}
 				}
 				else{
-					textInfo.innerHTML = "Something went wrong, check the transaction or retry."
-					approveButton.disabled = false;
+					textInfo.innerHTML = "Something went wrong, check the transaction or retry.";
+					img.data = "images/lovet.svg";
+					mintButton.disabled=false;
 				}
 			} catch (error){
 				console.log(`Error: ${error.message}`);
-				textInfo.innerHTML = "Something went wrong, check the transaction or retry."
-				approveButton.disabled = false;
-			}
-		});	
-	} else{
-		console.log("Error no address no mintAmount");
-		approveButton.disabled = false;
-	}    		
-};
-
-/* Load metadata & image from IPFS */
-const get_img = async (tokenId) => {
-	//Get metadata from contract
-	nft_contract.methods.tokenURI(tokenId).call().then( async function (result) {
-		const json_URL = IPFS_GATEWAY + result.substring(7);
-
-		//Get image from IPFS
-		try{
-			$.getJSON(json_URL, function(data) {
-				var img_URL = data.image;
-				const image_URI = IPFS_GATEWAY + img_URL.substring(7);		
-				textInfo.innerHTML = "Here is what you get: " + data.name + "<br>" +
-										"Type: " + data.attributes[0].value + ", Area: " + data.attributes[1].value  + "<br>" +
-										"I'm loading the image...";
-				//Load image
-				downloadingImage.onload = function() {
-					imgLink.href = image_URI;
-					img.data = this.src;
-					textInfo.innerHTML = "Here is what you get: " + data.name + "<br>" +
-											"Type: " + data.attributes[0].value + ", Area: " + data.attributes[1].value  + "<br>" +
-											"Done! View on OpenSea";
-					//Enable another mint
-					if (cost > 0){
-						approveButton.disabled = false;
-					}
-					else{
-						mintButton.disabled = false;
-					}
-				};			
-				downloadingImage.src = image_URI;
-			});
-		}catch(error){
-			console.log(`Error: ${error.message}`);
-			textInfo.innerHTML = "Sorry, something went wrong, check on OpenSea.";
-			img.data = "images/lovet.svg";
-		}	
-	});		
-}
-
-/* MINT */
-mintButton.onclick = async () => {
-	mintButton.disabled=true;
-	var tokenId;
-
-	const response = await refresh();
-	if( ethereum.selectedAddress.length > 0 && response){
-		//mint
-		const encodedFunction = web3.eth.abi.encodeFunctionCall(nft_mint_abi, [mintAmount]);
-		const transactionParameters = {
-			to: NFT_ADDRESS,
-			from: ethereum.selectedAddress,
-			data: encodedFunction
-		};              
-
-		// As with any RPC call, it may throw an error
-		try {
-			const txHash = await ethereum.request({
-			method: 'eth_sendTransaction',
-			params: [transactionParameters],
-			});
-			img.data = "images/logo_animated.svg";	
-			textInfo.innerHTML = "Waiting tx...";
-
-			//WAITING FOR SUCCESS TX
-			let txReceipt = null;
-			while (txReceipt == null) { // Waiting expectedBlockTime until the transaction is mined
-				txReceipt = await web3.eth.getTransactionReceipt(txHash);
-				await sleep(2000)
-			}
-
-			//GET TOKEN ID FROM TX
-			if (txReceipt.status){
-				textInfo.innerHTML="Your tx has been confirmed! I'm loading your new NFT...";				
-
-				//Get tokenId
-				if ( cost > 0 ) {
-					tokenId = parseInt(txReceipt.logs[2].topics[3],16);
-				}
-				else{
-					tokenId = parseInt(txReceipt.logs[0].topics[3],16);
-				}
-			}
-			else{
 				textInfo.innerHTML = "Something went wrong, check the transaction or retry.";
 				img.data = "images/lovet.svg";
 				mintButton.disabled=false;
 			}
-		} catch (error){
-			console.log(`Error: ${error.message}`);
-			textInfo.innerHTML = "Something went wrong, check the transaction or retry.";
+		} else{
+			console.log("Error no address no mintAmount");
 			img.data = "images/lovet.svg";
 			mintButton.disabled=false;
+		}    
+		//Wait totalSupply update and request the image from IPFS
+		let totSupply = 0; 
+		while (totSupply < tokenId) { 
+			await sleep(1000)
+			totSupply = await nft_contract.methods.getTotalSupply().call().then( async function (result) {
+				return result;
+			});
 		}
-	} else{
-		console.log("Error no address no mintAmount");
-		img.data = "images/lovet.svg";
-		mintButton.disabled=false;
-	}    
-	//Wait totalSupply update and request the image from IPFS
-	let totSupply = 0; 
-	while (totSupply < tokenId) { 
-		await sleep(1000)
-		totSupply = await nft_contract.methods.getTotalSupply().call().then( async function (result) {
-			return result;
-		});
+		get_img(tokenId);
 	}
-	get_img(tokenId);
-}
 
-/* Refresh contract status */
-const refresh = async () => {
-	//Get cost
-	await nft_contract.methods.cost().call().then(async function (result) {
-		cost = web3.utils.fromWei(result);
-		if(cost == 0){
-			approved = true;
-		}
-	});
-	//Get maxMintable
-	await nft_contract.methods.maxMintable().call().then(async function (result) {
-		maxMintable = parseInt(result);
-	});
-	//Get contract state
-	await nft_contract.methods.paused().call().then(async function (result) {
-		paused = result;
-	});
+	/* Refresh contract status */
+	const refresh = async () => {
+		//Get cost
+		await nft_contract.methods.cost().call().then(async function (result) {
+			cost = web3.utils.fromWei(result);
+			if(cost == 0){
+				approved = true;
+			}
+		});
+		//Get maxMintable
+		await nft_contract.methods.maxMintable().call().then(async function (result) {
+			maxMintable = parseInt(result);
+		});
+		//Get contract state
+		await nft_contract.methods.paused().call().then(async function (result) {
+			paused = result;
+		});
 
-	//Get Supply
-	const response_maxMintable = await nft_contract.methods.getTotalSupply().call().then(async function (result) {
-		if(result < maxMintable){
-			textPrice.innerHTML = "PRICE: " + cost + " WETH";
-			textSupply.innerHTML ="MINTED: " + result + "/" + maxMintable;
-			return true;
-		}
-		else{
-			textPrice.innerHTML = "SOLD OUT!";
-			textSupply.innerHTML = "SOLD OUT!";
-			textInfo.innerHTML = "SOLD OUT!";
-			return false;
-		}
-	});
-	//Get maxTokenWallet
-	const response_maxWallet = await nft_contract.methods.maxTokenWallet().call().then(async function (result) {
-		maxTokenWallet = parseInt(result);
-
-		//Get balanceOf user
-		const response = await nft_contract.methods.balanceOf(ethereum.selectedAddress).call().then(async function (result) {
-			tokenBalance = parseInt(result);
-			if ( tokenBalance >= maxTokenWallet ) {
-				textInfo.innerHTML = "You already have enough $LOVET!";
+		//Get Supply
+		const response_maxMintable = await nft_contract.methods.getTotalSupply().call().then(async function (result) {
+			if(result < maxMintable){
+				textPrice.innerHTML = "PRICE: " + cost + " WETH";
+				textSupply.innerHTML ="MINTED: " + result + "/" + maxMintable;
+				return true;
+			}
+			else{
+				textPrice.innerHTML = "SOLD OUT!";
+				textSupply.innerHTML = "SOLD OUT!";
+				textInfo.innerHTML = "SOLD OUT!";
 				return false;
 			}
-			else {
-				if(response_maxMintable && !paused){
-					if ( cost > 0) {
-						if(!approved){
-						textInfo.innerHTML = "Then click APPROVE to allow to spend WETH.";
-						approveButton.disabled = false;
+		});
+		//Get maxTokenWallet
+		const response_maxWallet = await nft_contract.methods.maxTokenWallet().call().then(async function (result) {
+			maxTokenWallet = parseInt(result);
+
+			//Get balanceOf user
+			const response = await nft_contract.methods.balanceOf(ethereum.selectedAddress).call().then(async function (result) {
+				tokenBalance = parseInt(result);
+				if ( tokenBalance >= maxTokenWallet ) {
+					textInfo.innerHTML = "You already have enough $LOVET!";
+					return false;
+				}
+				else {
+					if(response_maxMintable && !paused){
+						if ( cost > 0) {
+							if(!approved){
+							textInfo.innerHTML = "Then click APPROVE to allow to spend WETH.";
+							approveButton.disabled = false;
+							}
+						}
+						else{
+							textInfo.innerHTML = "Then click MINT.";
+							mintButton.disabled = false;
 						}
 					}
 					else{
-						textInfo.innerHTML = "Then click MINT.";
-						mintButton.disabled = false;
-					}
+						if (paused){
+							textInfo.innerHTML = "The minting is paused";
+						}
+					}					
+					return true;
 				}
-				else{
-					if (paused){
-						textInfo.innerHTML = "The minting is paused";
-					}
-				}					
-				return true;
-			}
+			});
+			return response;
 		});
-		return response;
+		return response_maxWallet && response_maxMintable;
+	};
+
+	/* Metamask Events */
+	ethereum.on('chainChanged', (chainId) => {
+	// Handle the new chain.
+	// Correctly handling chain changes can be complicated.
+	// We recommend reloading the page unless you have good reason not to.
+	window.location.reload();
 	});
-	return response_maxWallet && response_maxMintable;
-};
+	ethereum.on("accountsChanged", accounts => {
+	if (accounts.length > 0){
+		console.log(`Account connected: ${accounts[0]}`);
+		//Display address
+		addressDisplay.innerHTML = "Address: " + smallAddressFormat();
+		refresh();
+	}
+	else
+		console.log("Account disconnected");
+	});
 
-/* Metamask Events */
-ethereum.on('chainChanged', (chainId) => {
-// Handle the new chain.
-// Correctly handling chain changes can be complicated.
-// We recommend reloading the page unless you have good reason not to.
-window.location.reload();
-});
-ethereum.on("accountsChanged", accounts => {
-if (accounts.length > 0){
-	console.log(`Account connected: ${accounts[0]}`);
-	//Display address
-	addressDisplay.innerHTML = "Address: " + smallAddressFormat();
-	refresh();
+	MetaMaskClientCheck();
 }
-else
-	console.log("Account disconnected");
-});
-
 
 window.addEventListener('DOMContentLoaded', initialize)
 
@@ -388,4 +386,3 @@ new Vivus('logo',
 $(window).on("load",function(){
 	$(".loader-wrapper").fadeOut("slow");
 });
-
